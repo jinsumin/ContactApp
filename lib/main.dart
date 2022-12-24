75939f293473bb3e145991f79506ca5681c5fff7
jinsumin <suminjin@naver.com>
2022-12-25 01:42:33 +0900

    bottom nav bar & body pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
-        bottomNavigationBar: BottomAppBar(),
-      )
+    return const MaterialApp(home: MainNavigation());
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( backgroundColor: Colors.cyan,),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.phone),
+            label: 'Phone',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.contact_page),
+            label: 'Contact',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        Container(
+          color: Colors.cyan,
+          alignment: Alignment.center,
+          child: const Text('Page 1'),
+        ),
+        Container(
+          color: Colors.green,
+          alignment: Alignment.center,
+          child: const Text('Page 2'),
+        ),
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text('Page 3'),
+        ),
+      ][currentPageIndex],
     );
   }
 }
